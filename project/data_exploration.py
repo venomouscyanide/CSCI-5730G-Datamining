4deeda0fb73e1274f6d383eedc921982ee52b1fe
Paul Louis <paul.louis@ontariotechu.net>
2021-11-27 22:51:56 -0500

    Colab code progress

--- project/data_exploration.py
+++ project/data_exploration.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
 from torch.nn import CrossEntropyLoss, MSELoss, NLLLoss
 from torch.utils.data import TensorDataset, DataLoader
 
-from project.models import RNNPred
+from project.models import RNNPred, RNNModel, LSTMModel
 
 import warnings
 
@@ -17,14 +18,17 @@ warnings.simplefilter(action="ignore")
 
 class TrainIndividual:
     def train(self, train_data, labels):
-        batch_size = 16
-        rnn_model = RNNPred(input_dim=10, hidden_size=64, no_of_hidden_layers=3, output_size=1)
+        batch_size = 64
+        rnn_model = LSTMModel(input_dim=63, hidden_dim=64, layer_dim=3, output_dim=1, dropout_prob=0.5)
         optimizer = torch.optim.Adam(rnn_model.parameters(), lr=0.001, weight_decay=1e-6)
         loss_fn = MSELoss()
 
-        x = torch.from_numpy(train_data.values).double()
-        train_dataset = TensorDataset(x, torch.tensor(labels, dtype=torch.double))
-        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, drop_last=True)
+
+        train_dataset = TensorDataset(torch.tensor(train_data, dtype=torch.double), torch.tensor(labels, dtype=torch.double))
+        train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, drop_last=True)
+
+        opt = Optimization(model=rnn_model, loss_fn=loss_fn, optimizer=optimizer)
+        opt.train(train_loader, val_loader=None, batch_size=batch_size, n_epochs=5, n_features=63)
 
         for epoch in range(10):
             rnn_model.zero_grad()
@@ -32,11 +36,11 @@ class TrainIndividual:
             for x, y in train_loader:
                 # x = x.reshape(10, 1)
                 # y = torch.tensor([y], dtype=torch.double)
-                x = x.view([16, -1, 10])
+                x = x.view([64, -1, 10])
                 output = rnn_model(x)
                 # print(output[-1])
                 # loss += loss_fn(y, output[-1])
-                loss = loss_fn(y, output.reshape(16, 1, 1))
+                loss = loss_fn(y, output.reshape(64, 1, 1))
                 epoch_loss += loss.item()
                 loss.backward()
                 optimizer.step()
@@ -108,6 +112,22 @@ class Cleaning:
         enhanced_train_data["state"] = oe_state.fit_transform(enhanced_train_data[["state"]])
         enhanced_train_data["type_y"] = oe_type_y.fit_transform(enhanced_train_data[["type_y"]])
         enhanced_train_data.set_index('date', inplace=True)
+        enhanced_train_data.index = pd.to_datetime(enhanced_train_data.index)
+        enhanced_train_data = (
+            enhanced_train_data
+                .assign(day=enhanced_train_data.index.day)
+                .assign(month=enhanced_train_data.index.month)
+                .assign(day_of_week=enhanced_train_data.index.dayofweek)
+                .assign(week_of_year=enhanced_train_data.index.week)
+        )
+
+        def onehot_encode_pd(df, cols):
+            for col in cols:
+                dummies = pd.get_dummies(df[col], prefix=col)
+
+            return pd.concat([df, dummies], axis=1).drop(columns=cols)
+
+        enhanced_train_data = onehot_encode_pd(enhanced_train_data, ['month', 'day', 'day_of_week', 'week_of_year'])
 
         grouped_data = enhanced_train_data.groupby(by=['store_nbr', 'family'])
 
@@ -115,6 +135,10 @@ class Cleaning:
         rows_to_train_on.drop(columns=['id', 'family', 'store_nbr'], inplace=True)
         y = rows_to_train_on.sales
         rows_to_train_on.drop(columns=['sales'])
+
+        scaler = MinMaxScaler()
+        rows_to_train_on = scaler.fit_transform(rows_to_train_on)
+        y = scaler.fit_transform(y.values.reshape(1684, 1))
         return rows_to_train_on, y
 
     def clean(self, base_path):
@@ -208,6 +232,159 @@ def run(base_path):
     TrainIndividual().train(train_data, labels)
     # train_data, labels = Cleaning().clean(base_path)
     # TrainRnn().train(train_data, labels)
+
+device = 'cpu'
+class Optimization:
+    """Optimization is a helper class that allows training, validation, prediction.
+
+    Optimization is a helper class that takes model, loss function, optimizer function
+    learning scheduler (optional), early stopping (optional) as inputs. In return, it
+    provides a framework to train and validate the models, and to predict future values
+    based on the models.
+
+    Attributes:
+        model (RNNModel, LSTMModel, GRUModel): Model class created for the type of RNN
+        loss_fn (torch.nn.modules.Loss): Loss function to calculate the losses
+        optimizer (torch.optim.Optimizer): Optimizer function to optimize the loss function
+        train_losses (list[float]): The loss values from the training
+        val_losses (list[float]): The loss values from the validation
+        last_epoch (int): The number of epochs that the models is trained
+    """
+
+    def __init__(self, model, loss_fn, optimizer):
+        """
+        Args:
+            model (RNNModel, LSTMModel, GRUModel): Model class created for the type of RNN
+            loss_fn (torch.nn.modules.Loss): Loss function to calculate the losses
+            optimizer (torch.optim.Optimizer): Optimizer function to optimize the loss function
+        """
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.train_losses = []
+        self.val_losses = []
+
+    def train_step(self, x, y):
+        """The method train_step completes one step of training.
+
+        Given the features (x) and the target values (y) tensors, the method completes
+        one step of the training. First, it activates the train mode to enable back prop.
+        After generating predicted values (yhat) by doing forward propagation, it calculates
+        the losses by using the loss function. Then, it computes the gradients by doing
+        back propagation and updates the weights by calling step() function.
+
+        Args:
+            x (torch.Tensor): Tensor for features to train one step
+            y (torch.Tensor): Tensor for target values to calculate losses
+
+        """
+        # Sets model to train mode
+        self.model.train()
+
+        # Makes predictions
+        yhat = self.model(x)
+
+        # Computes loss
+        loss = self.loss_fn(y, yhat)
+
+        # Computes gradients
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 5)
+
+        # Updates parameters and zeroes gradients
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        # Returns the loss
+        return loss.item()
+
+    def train(self, train_loader, val_loader, batch_size=64, n_epochs=50, n_features=1):
+        """The method train performs the model training
+
+        The method takes DataLoaders for training and validation datasets, batch size for
+        mini-batch training, number of epochs to train, and number of features as inputs.
+        Then, it carries out the training by iteratively calling the method train_step for
+        n_epochs times. If early stopping is enabled, then it  checks the stopping condition
+        to decide whether the training needs to halt before n_epochs steps. Finally, it saves
+        the model in a designated file path.
+
+        Args:
+            train_loader (torch.utils.data.DataLoader): DataLoader that stores training data
+            val_loader (torch.utils.data.DataLoader): DataLoader that stores validation data
+            batch_size (int): Batch size for mini-batch training
+            n_epochs (int): Number of epochs, i.e., train steps, to train
+            n_features (int): Number of feature columns
+
+        """
+        model_path = f'{self.model}_{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+
+        for epoch in range(1, n_epochs + 1):
+            batch_losses = []
+            for x_batch, y_batch in train_loader:
+                x_batch = x_batch.view([batch_size, -1, n_features]).to(device)
+                y_batch = y_batch.to(device)
+                loss = self.train_step(x_batch, y_batch)
+                batch_losses.append(loss)
+            training_loss = np.mean(batch_losses)
+            self.train_losses.append(training_loss)
+
+            # with torch.no_grad():
+            #     batch_val_losses = []
+            #     for x_val, y_val in val_loader:
+            #         x_val = x_val.view([batch_size, -1, n_features]).to(device)
+            #         y_val = y_val.to(device)
+            #         self.model.eval()
+            #         yhat = self.model(x_val)
+            #         val_loss = self.loss_fn(y_val, yhat).item()
+            #         batch_val_losses.append(val_loss)
+            #     validation_loss = np.mean(batch_val_losses)
+            #     self.val_losses.append(validation_loss)
+
+            if (epoch <= 10) | (epoch % 50 == 0):
+                print(
+                    f"[{epoch}/{n_epochs}] Training loss: {training_loss:.4f}\t Validation loss: "
+                )
+
+        # torch.save(self.model.state_dict(), model_path)
+
+    def evaluate(self, test_loader, batch_size=1, n_features=1):
+        device = 'cpu'
+        """The method evaluate performs the model evaluation
+
+        The method takes DataLoaders for the test dataset, batch size for mini-batch testing,
+        and number of features as inputs. Similar to the model validation, it iteratively
+        predicts the target values and calculates losses. Then, it returns two lists that
+        hold the predictions and the actual values.
+
+        Note:
+            This method assumes that the prediction from the previous step is available at
+            the time of the prediction, and only does one-step prediction into the future.
+
+        Args:
+            test_loader (torch.utils.data.DataLoader): DataLoader that stores test data
+            batch_size (int): Batch size for mini-batch training
+            n_features (int): Number of feature columns
+
+        Returns:
+            list[float]: The values predicted by the model
+            list[float]: The actual values in the test set.
+
+        """
+        with torch.no_grad():
+            predictions = []
+            values = []
+            for x_test, y_test in test_loader:
+                x_test = x_test.view([batch_size, -1, n_features]).to(device)
+                y_test = y_test.to(device)
+                self.model.eval()
+                yhat = self.model(x_test)
+                predictions.append(yhat.to(device).detach().numpy())
+                values.append(y_test.to(device).detach().numpy())
+
+        return predictions, values
+
+
 
 
 if __name__ == '__main__':
--- project/models.py
+++ project/models.py
@@ -25,6 +25,16 @@ class RNNPred(nn.Module):
 
 class RNNModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        """The __init__ method that initiates an RNN instance.
+
+        Args:
+            input_dim (int): The number of nodes in the input layer
+            hidden_dim (int): The number of nodes in each layer
+            layer_dim (int): The number of layers in the network
+            output_dim (int): The number of nodes in the output layer
+            dropout_prob (float): The probability of nodes being dropped out
+
+        """
         super(RNNModel, self).__init__()
 
         # Defining the number of layers and the nodes in each layer
@@ -34,13 +44,22 @@ class RNNModel(nn.Module):
         # RNN layers
         self.rnn = nn.RNN(
             input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
-        )
+        ).double()
         # Fully connected layer
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.fc = nn.Linear(hidden_dim, output_dim, dtype=torch.double)
 
     def forward(self, x):
+        """The forward method takes input tensor x and does forward propagation
+
+        Args:
+            x (torch.Tensor): The input tensor of the shape (batch size, sequence length, input_dim)
+
+        Returns:
+            torch.Tensor: The output tensor of the shape (batch size, output_dim)
+
+        """
         # Initializing hidden state for first input with zeros
-        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, dtype=torch.double).requires_grad_()
 
         # Forward propagation by passing in the input and hidden state into the model
         out, h0 = self.rnn(x, h0.detach())
@@ -88,3 +107,77 @@ class Model(nn.Module):
         # We'll send the tensor holding the hidden state to the device we specified earlier as well
         hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
         return hidden
+
+
+class LSTMModel(nn.Module):
+    """LSTMModel class extends nn.Module class and works as a constructor for LSTMs.
+
+       LSTMModel class initiates a LSTM module based on PyTorch's nn.Module class.
+       It has only two methods, namely init() and forward(). While the init()
+       method initiates the model with the given input parameters, the forward()
+       method defines how the forward propagation needs to be calculated.
+       Since PyTorch automatically defines back propagation, there is no need
+       to define back propagation method.
+
+       Attributes:
+           hidden_dim (int): The number of nodes in each layer
+           layer_dim (str): The number of layers in the network
+           lstm (nn.LSTM): The LSTM model constructed with the input parameters.
+           fc (nn.Linear): The fully connected layer to convert the final state
+                           of LSTMs to our desired output shape.
+
+    """
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        """The __init__ method that initiates a LSTM instance.
+
+        Args:
+            input_dim (int): The number of nodes in the input layer
+            hidden_dim (int): The number of nodes in each layer
+            layer_dim (int): The number of layers in the network
+            output_dim (int): The number of nodes in the output layer
+            dropout_prob (float): The probability of nodes being dropped out
+
+        """
+        super(LSTMModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        # LSTM layers
+        self.lstm = nn.LSTM(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        ).double()
+
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim, dtype=torch.double)
+
+    def forward(self, x):
+        """The forward method takes input tensor x and does forward propagation
+
+        Args:
+            x (torch.Tensor): The input tensor of the shape (batch size, sequence length, input_dim)
+
+        Returns:
+            torch.Tensor: The output tensor of the shape (batch size, output_dim)
+
+        """
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, dtype=torch.double).requires_grad_()
+
+        # Initializing cell state for first input with zeros
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, dtype=torch.double).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        # Forward propagation by passing in the input, hidden state, and cell state into the model
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+
+        return out
